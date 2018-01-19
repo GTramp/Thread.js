a5bde4ce682769c3fafb1fa6ca7ff057e1bf0b5a
command <wg_workshop@outlook.com>
2018-01-19 19:14:36 +0800

    Nom publish

--- example/react-demo/src/index.js
+++ example/react-demo/src/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 import thread from './thread'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
 
 // demo ========
-thread.async (() => {
-  let j = 0
-  for (let i = 0; i < 10; i++) {
-    console.log(`async :: ${i}`)
-    j++
-  }
-  return j
-}, resp => {
-  console.log(resp)
-}, error => {
-  console.error(error)
-})
+(function() {
+  thread.async(
+    () => {
+      let j = 0
+      for (let i = 0; i < 10; i++) {
+        console.log(`async :: ${i}`)
+        j++
+      }
+      return j
+    },
+    resp => {
+      console.log(resp)
+    },
+    error => {
+      console.error(error)
+    }
+  )
+})()
